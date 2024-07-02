0326c414a05b3e7d18ed3600244a9d9c70f9db91
JayeshKankaria <101986220+JayeshKankaria@users.noreply.github.com>
2024-07-02 14:28:00 +0530

    Time: 8 ms (97.88%), Space: 45 MB (70.12%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -5,16 +5,16 @@ class Solution {
         Arrays.sort(s);
         int sl=0;
         int gl =0;
-        int count=0;
+        //int count=0;
         while(sl<s.length && gl<g.length)
         {
             if(s[sl]>=g[gl])
             {
-                count++;
+                //count++;
                 gl++;
             }
             sl++; 
         }
-        return count;
+        return gl;
     }
 }
