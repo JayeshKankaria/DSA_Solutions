2bdd3097a712457eff1f3503f0d01679ca970065
JayeshKankaria <101986220+JayeshKankaria@users.noreply.github.com>
2024-07-24 17:03:31 +0530

    Time: 4 ms (19.51%), Space: 62.5 MB (5.93%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -3,15 +3,13 @@ class Solution
     public int maxJump(int[] stones) 
     {
         int n = stones.length;
-        if(n==2)
+        int dp[] = new int[n];
+        dp[0]=0;
+        dp[1]=stones[1]-stones[0];
+        for(int i =2;i<n;i++)
         {
-            return stones[n-1];
+            dp[i]=Math.max(dp[i-1],stones[i]-stones[i-2]);
         }
-        int md=0;
-        for(int i=0;i<n-2;i++)
-        {
-            md = Math.max(md,stones[i+2]-stones[i]);
-        }
-        return md; 
+        return dp[n-1];
     }
 }
